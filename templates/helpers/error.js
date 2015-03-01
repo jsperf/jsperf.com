5369b2b7e949631abcb10280baa9dfb10a67e08c
Max Beatty <max@beatty.me>
2015-03-01 11:07:44 -0800

    use es6 template for error helper

--- templates/helpers/error.js
+++ templates/helpers/error.js
@@ -6,8 +6,7 @@ module.exports = function(msg, options) {
   var result;
   var tag = options.hash.tag || "span";
   if (msg) {
-    result = new Handlebars.SafeString(
-      "<" + tag + " class=\"error\">" + msg + "</" + tag + ">");
+    result = new Handlebars.SafeString(`<${tag} class="error">${msg}</${tag}>`);
   }
 
   return result;
