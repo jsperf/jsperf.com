c650f5e4b9e7c530018d72d80626473d4c3f8c3f
Max Beatty <max@beatty.me>
2015-03-08 15:19:00 -0700

    add tests for regex

---
+++ test/server/lib/regex.js
@@ -0,0 +1,63 @@
+"use strict";
+
+var Lab = require("lab");
+var Code = require("code");
+
+var regex = require("../../../server/lib/regex");
+
+var lab = exports.lab = Lab.script();
+
+lab.experiment("Regex Lib", function() {
+
+  lab.experiment("url", function() {
+
+    lab.test("is a string", function(done) {
+
+      Code.expect(regex.url).to.be.a.string();
+
+      done();
+    });
+
+    lab.test("matches valid url", function(done) {
+      var re = new RegExp(regex.url);
+
+      Code.expect(re.test("http://user:pass@www.example.com:80/path")).to.be.true();
+
+      done();
+    });
+
+    lab.test("does not match invalid url", function(done) {
+      var re = new RegExp(regex.url);
+
+      Code.expect(re.test("this.is.not.a.url")).to.be.false();
+
+      done();
+    });
+  });
+
+  lab.experiment("slug", function() {
+
+    lab.test("is a string", function(done) {
+
+      Code.expect(regex.slug).to.be.a.string();
+
+      done();
+    });
+
+    lab.test("matches valid slug", function(done) {
+      var re = new RegExp(regex.slug);
+
+      Code.expect(re.test("aB3-")).to.be.true();
+
+      done();
+    });
+
+    lab.test("does not match invalid slug", function(done) {
+      var re = new RegExp(regex.slug);
+
+      Code.expect(re.test("!@#$")).to.be.false();
+
+      done();
+    });
+  });
+});
