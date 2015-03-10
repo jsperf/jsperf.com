9095e2d1a6d01921638fe3f064ed7c045062d4c7
Max Beatty <max@beatty.me>
2015-03-09 21:29:05 -0700

    add tests for tests repository

--- server/repositories/tests.js
+++ server/repositories/tests.js
@@ -5,35 +5,29 @@ var db = require("../lib/db");
 var table = "tests";
 
 module.exports = {
-  bulkCreate: function(pageID, tests) {
-    return new Promise(function(resolve, reject) {
-      var conn = db.createConnection();
+  bulkCreate: function(pageID, tests, cb) {
+    var conn = db.createConnection();
 
-      var columns = ["pageID", "title", "defer", "code"];
+    var columns = ["pageID", "title", "defer", "code"];
 
-      var values = [];
-      for(var i = 0, tl = tests.length; i < tl; i++) {
-        var test = tests[i];
-        values.push(`(${pageID}, ${conn.escape(test.title)}, ${conn.escape(test.defer)}, ${conn.escape(test.code)})`);
+    var values = [];
+    for(var i = 0, tl = tests.length; i < tl; i++) {
+      var test = tests[i];
+      values.push(`(${pageID}, ${conn.escape(test.title)}, ${conn.escape(test.defer)}, ${conn.escape(test.code)})`);
+    }
+
+    values = values.join(", ");
+
+    conn.query("INSERT INTO ?? (??) VALUES " + values, [table, columns], function(err, result) {
+      if (err) {
+        cb(err);
+      } else if (result.affectedRows !== tests.length) {
+        cb(new Error("Not all tests inserted"));
+      } else {
+        cb(null);
       }
-
-      values = values.join(", ");
-
-
-
-      conn.query("INSERT INTO ?? (??) VALUES " + values, [table, columns], function(err, result) {
-        if (err) {
-          reject(err);
-        }
-
-        if (result.affectedRows !== tests.length) {
-          reject(new Error("Not all tests inserted"));
-        }
-
-        resolve();
-      });
-
-      conn.end();
     });
+
+    conn.end();
   }
 };
---
+++ test/server/repositories/tests.js
@@ -0,0 +1,111 @@
+"use strict";
+
+var Lab = require("lab");
+var Code = require("code");
+var proxyquire = require("proxyquire");
+var sinon = require("sinon");
+
+var dbStub = {};
+
+var tests = proxyquire("../../../server/repositories/tests", {
+  "../lib/db": dbStub
+});
+
+var lab = exports.lab = Lab.script();
+
+lab.experiment("Tests Repository", function() {
+
+  lab.experiment("bulkCreate", function() {
+    var queryStub;
+    var pageID;
+    var t;
+
+    lab.before(function(done) {
+      t = [
+        {
+          title: "t1",
+          defer: "n",
+          code: "a = 1"
+        }, {
+          title: "t2",
+          defer: "n",
+          code: "a = 2"
+        }
+      ];
+
+      pageID = 1;
+
+      dbStub.createConnection = function() {
+
+        return {
+          query: queryStub,
+          escape: function(val) {
+            return "`" + val + "`";
+          },
+          end: function() {}
+        };
+      };
+
+      done();
+    });
+
+    lab.beforeEach(function(done) {
+      queryStub = sinon.stub();
+
+      done();
+    });
+
+    lab.test("inserts multiple values", function(done) {
+      queryStub.callsArgWith(2, null, { affectedRows: t.length });
+
+      tests.bulkCreate(pageID, t, function(err) {
+
+        Code.expect(err).to.be.null();
+        Code.expect(
+          queryStub.calledWith(
+            "INSERT INTO ?? (??) VALUES (1, `t1`, `n`, `a = 1`), (1, `t2`, `n`, `a = 2`)",
+            [
+              "tests",
+              ["pageID", "title", "defer", "code"]
+            ]
+          )
+        ).to.be.true();
+
+
+        done();
+      });
+
+    });
+
+    lab.test("returns an error when not enough rows inserted", function(done) {
+
+      queryStub.callsArgWith(2, null, { affectedRows: t.length - 1 });
+
+      tests.bulkCreate(pageID, t, function(err) {
+
+        Code.expect(err).to.be.instanceof(Error);
+        Code.expect(err.message).to.equal("Not all tests inserted");
+
+        done();
+      });
+
+    });
+
+    lab.test("returns an error when not enough rows inserted", function(done) {
+      var testErrMsg = "testing";
+      var testErr = new Error(testErrMsg);
+
+      queryStub.callsArgWith(2, testErr);
+
+      tests.bulkCreate(pageID, t, function(err) {
+
+        Code.expect(err).to.be.instanceof(Error);
+        Code.expect(err.message).to.equal(testErrMsg);
+
+        done();
+      });
+
+    });
+
+  });
+});
