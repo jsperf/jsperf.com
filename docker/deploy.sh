800270968eb011e104d6d69a6c99b7e2019af42d
Andrew Silluron <asilluron@users.noreply.github.com>
2016-01-30 13:13:06 -0800

    Docker deploy script
    Bring down old services and bring up the latest.

---
+++ docker/deploy.sh
@@ -0,0 +1,11 @@
+docker pull jsperf/jsperf.com
+dbservice=$(docker ps --filter name=db  --format "{{.Names}}")
+timestamp=$(date +'%Y%m%d-%H-%M-%S')
+services=$(docker ps --filter name=web  --format "{{.Names}}")
+enum=1
+for service in $services; do
+  docker stop $service
+  docker rm $service
+  docker run -d --name 'jsperfcom_web_'$timestamp'_'$enum --link $dbservice:db --env-file .env --env SERVICE_3000_CHECK_HTTP=/health --env SERVICE_3000_CHECK_INTERVAL=1s jsperfcom_web
+  enum=$((enum+1))
+done
