5f96556282ae5bc5da94075db6dedf514488a621
Andrew Silluron <asilluron@users.noreply.github.com>
2016-04-09 16:22:13 -0700

    working config!

--- docker/deploy.sh
+++ docker/deploy.sh
@@ -6,6 +6,6 @@ enum=1
 for service in $services; do
   docker stop $service
   docker rm $service
-  docker run -d --name 'jsperfcom_web_'$timestamp'_'$enum --link $dbservice:db --env-file .env --env SERVICE_3000_CHECK_HTTP=/health --env SERVICE_3000_CHECK_INTERVAL=1s jsperfcom_web
+  docker run -d --name 'jsperfcom_web_'$timestamp'_'$enum --link $dbservice:db --env-file /etc/environment --env SERVICE_3000_CHECK_HTTP=/health --env SERVICE_3000_CHECK_INTERVAL=1s jsperfcom_web
   enum=$((enum+1))
 done
--- docker/setup/lb.sh
+++ docker/setup/lb.sh
@@ -1,12 +1,23 @@
 docker run -d \
 --name=consul \
 --net=host \
+-p 8500:8500 \
+-p 53:8600/udp \
+-p 8400:8400 \
 gliderlabs/consul-server:0.6 \
 -bootstrap \
--bind=127.0.0.1
+-advertise $DOCKER_IP \
+-client 0.0.0.0
+
 docker run -d \
 --name=registrator \
 --net=host \
 --volume="/var/run/docker.sock:/tmp/docker.sock" \
 gliderlabs/registrator:v6 \
-consul://127.0.0.1:8500
+-internal consul://localhost:8500
+
+docker run -d \
+--name=loadbalancer \
+-p 80:80 \
+--net=host \
+jsperf/jsperf-load-balancer
