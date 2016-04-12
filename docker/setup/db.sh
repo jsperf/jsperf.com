703a3356941bf1d9f01dc8912147a51ade4085c7
Andrew Silluron <asilluron@users.noreply.github.com>
2016-04-11 19:46:08 -0700

    Clean up config. Ensure docker-compose works for production.

--- docker/setup/db.sh
+++
@@ -1,7 +0,0 @@
-docker run -d --name jsperfcom_db_1 \
--v /data-jsperf-mysql \
---env MYSQL_PASSWORD \
---env MYSQL_RANDOM_ROOT_PASSWORD='true' \
---env MYSQL_DATABASE='jsperf' \
---env MYSQL_USER='jsperf' \
-mysql
