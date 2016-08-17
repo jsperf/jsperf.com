9ab1e17700cee18ec05278ecbb873ad345e22830
Max Beatty <max@beatty.me>
2016-08-17 14:34:29 -0700

    ignore illegal syntax when highlighting

--- templates/helpers/highlight.js
+++ templates/helpers/highlight.js
@@ -2,6 +2,6 @@ var Handlebars = require('handlebars');
 var hljs = require('highlight.js');
 
 module.exports = function (code, lang) {
-  var result = hljs.highlight(lang, code);
+  var result = hljs.highlight(lang, code, true);
   return new Handlebars.SafeString('<pre><code class="' + lang + '">' + result.value + '</code></pre>');
 };
