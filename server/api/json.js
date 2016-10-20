048242ca6ec808d80d8da7db63473185c20852d3
OrKoN <alexei.rudenko@gmail.com>
2016-10-20 23:04:26 +0200

    Initial api for getting page content

--- server/api/json.js
+++ server/api/json.js
@@ -1,9 +1,68 @@
+var pagesService = require('../services/pages');
+
+var mapTest = function (test) {
+  return {
+    testID: test.testID,
+    title: test.title,
+    code: test.code,
+    defer: test.defer
+  };
+};
+
+var mapPage = function (page) {
+  return {
+    id: page.id,
+    slug: page.slug,
+    revision: page.revision,
+    title: page.title,
+    info: page.info,
+    setup: page.setup,
+    teardown: page.teardown,
+    published: page.published,
+    updated: page.updated,
+    maxRev: page.maxRev
+  };
+};
+
+var mapRevision = function (revision) {
+  return {
+    author: revision.author,
+    revision: revision.revision,
+    published: revision.published,
+    updated: revision.updated,
+    maxRev: revision.maxRev,
+    title: revision.title
+  };
+};
+
+var mapComment = function (comment) {
+  return {
+    id: comment.id,
+    author: comment.author,
+    published: comment.published,
+    content: comment.content
+  };
+};
+
 exports.register = function (server, options, next) {
   server.route({
     method: 'GET',
-    path: '/api/json',
+    path: '/{slug}/{revision}.json',
     handler: function (request, reply) {
-      reply({ content: 'test' });
+      return pagesService.getBySlug(request.params.slug, request.params.revision)
+        .then(function (results) {
+          reply({
+            page: mapPage(results[0]),
+            tests: results[1].map(mapTest),
+            revisions: results[2].map(mapRevision),
+            comments: results[3].map(mapComment)
+          })
+          .type('application/json')
+          .code(200);
+        })
+        .catch(function (error) {
+          reply(error);
+        });
     }
   });
 
--- test/unit/server/api/json.js
+++ test/unit/server/api/json.js
@@ -2,19 +2,20 @@ var Lab = require('lab');
 var Code = require('code');
 var Hapi = require('hapi');
 
-var Config = require('../../../../config');
-
-var JsonPlugin = require('../../../../server/api/json');
+var proxyquire = require('proxyquire');
+var pagesServiceStub = {};
+var JsonPlugin = proxyquire('../../../../server/api/json', {
+  '../services/pages': pagesServiceStub
+});
 
 var lab = exports.lab = Lab.script();
 var request, server;
+var expect = Code.expect;
 
 lab.beforeEach(function (done) {
   var plugins = [ JsonPlugin ];
   server = new Hapi.Server();
-  server.connection({
-    port: Config.get('/port/web')
-  });
+  server.connection();
   server.register(plugins, done);
 });
 
@@ -22,18 +23,73 @@ lab.experiment('json', function () {
   lab.beforeEach(function (done) {
     request = {
       method: 'GET',
-      url: '/api/json'
+      url: '/test/1.json'
     };
 
     done();
   });
 
-  lab.test('it returns object with "content" key and "test" value', function (done) {
+  lab.test('it returns page data', function (done) {
+    const data = [
+      { // page
+        id: 1,
+        slug: 'test',
+        revision: 1,
+        title: 'title',
+        info: '',
+        setup: '',
+        teardown: '',
+        published: 'n',
+        updated: '',
+        maxRev: 1
+      }, [ // tests
+        {
+          testID: 1,
+          title: 'Test',
+          code: 'Test',
+          defer: 'n'
+        }
+      ], [ // revisions
+        {
+          author: '',
+          revision: 1,
+          published: '',
+          updated: '',
+          maxRev: 1,
+          title: 'title'
+        }
+      ], [ // comments
+        {
+          id: 1,
+          author: '',
+          published: '',
+          content: ''
+        }
+      ]
+    ];
+    pagesServiceStub.getBySlug = function (cnt) {
+      return Promise.resolve(data);
+    };
     server.inject(request, function (response) {
-      Code.expect(response.statusCode).to.equal(200);
-      Code.expect(response.headers['content-type']).to.equal('application/json; charset=utf-8');
-      Code.expect(response.result.content).to.equal('test');
+      expect(response.statusCode).to.equal(200);
+      expect(response.headers['content-type']).to.equal('application/json; charset=utf-8');
+      expect(response.result.page).to.equal(data[0]);
+      expect(response.result.tests).to.equal(data[1]);
+      expect(response.result.revisions).to.equal(data[2]);
+      expect(response.result.comments).to.equal(data[3]);
+      done();
+    });
+  });
 
+  lab.test('it returns errors', function (done) {
+    pagesServiceStub.getBySlug = function (cnt) {
+      return Promise.reject(new Error('DB is down'));
+    };
+    server.inject(request, function (response) {
+      expect(response.headers['content-type']).to.equal('application/json; charset=utf-8');
+      expect(response.result.statusCode).to.equal(500);
+      expect(response.result.error).to.equal('Internal Server Error');
+      expect(response.result.message).to.equal('An internal server error occurred');
       done();
     });
   });
