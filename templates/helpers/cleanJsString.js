766cf6bcb1cef5241168b2bc8a6ee18d691976f6
Max Beatty <max@beatty.me>
2016-12-10 11:23:45 -0800

    closes #287

--- templates/helpers/cleanJsString.js
+++ templates/helpers/cleanJsString.js
@@ -1,11 +1,11 @@
 var jsesc = require('jsesc');
 
-// use jscs to make our strings javascript ready
+// use jsesc to make our strings javascript ready
 // remove script tags so it doesn't mess with our parent <script> tags
 module.exports = (value) => jsesc(
   value,
   {
     wrap: true,
-    escapeEtago: true
+    isScriptContext: true
   }
 );
